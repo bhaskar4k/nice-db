b6847f41cbe73f4a935493e0ee81e796f176a694
bhaskar4k <bhaskarndy@gmail.com>
2025-05-26 22:12:59 +0530

    Updated

--- command-control/control-file/command-routing.cpp
+++ command-control/control-file/command-routing.cpp
@@ -36,44 +36,53 @@ void HandleCommand(SOCKET &clientSocket) {
             bool got_all_info_to_create_a_table = true;
             string table_name = "";
             int table_columns = 0;
-            
-            if ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0){
-                const char* prompt = "Table Name : ";
-                send(clientSocket, prompt, strlen(prompt), 0);
+
+            // Ask for table name
+            const char* namePrompt = "Table Name: ";
+            send(clientSocket, namePrompt, strlen(namePrompt), 0);
+
+            bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+            if (bytesRead > 0) {
+                buffer[bytesRead] = '\0';
+                string name(buffer);
+                name.erase(name.find_last_not_of(" \n\r\t") + 1);
+                table_name = name;
+
+                // Ask for column count
+                const char* colPrompt = "Table Columns: ";
+                send(clientSocket, colPrompt, strlen(colPrompt), 0);
 
                 bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
                 if (bytesRead > 0) {
                     buffer[bytesRead] = '\0';
-                    string name(buffer);
-                    name.erase(name.find_last_not_of(" \n\r\t") + 1);
-                    table_name = name;
+                    string columnStr(buffer);
+                    columnStr.erase(columnStr.find_last_not_of(" \n\r\t") + 1);
 
-                    const char* prompt = "Table Columns : ";
-                    send(clientSocket, prompt, strlen(prompt), 0);
-
-                    if ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
-                        buffer[bytesRead] = '\0';
-                        string column(buffer);
-                        column.erase(column.find_last_not_of(" \n\r\t") + 1);
-                        table_columns = stoi(column);
-                    } else {
-                        const char* error = "No column count received.\n";
-                        send(clientSocket, error, strlen(error), 0);
+                    try {
+                        table_columns = stoi(columnStr);
+                    } catch (...) {
                         got_all_info_to_create_a_table = false;
-                    } 
+                        const char* err = "Invalid column count.\n";
+                        send(clientSocket, err, strlen(err), 0);
+                    }
                 } else {
-                    const char* error = "No table name received.\n";
-                    send(clientSocket, error, strlen(error), 0);
                     got_all_info_to_create_a_table = false;
+                    const char* err = "No column count received.\n";
+                    send(clientSocket, err, strlen(err), 0);
                 }
+            } else {
+                got_all_info_to_create_a_table = false;
+                const char* err = "No table name received.\n";
+                send(clientSocket, err, strlen(err), 0);
+            }
 
-                if(got_all_info_to_create_a_table){
-                    string response = "A Nice table [" + table_name + "] created with [" + to_string(table_columns) + "] columns.\n";
-                    send(clientSocket, response.c_str(), response.length(), 0);
-                }else{
-                    string response = "Didn't get all necessery data to create a nice table.\n";
-                    send(clientSocket, response.c_str(), response.length(), 0);
-                }
+            // Final response
+            if (got_all_info_to_create_a_table) {
+                string response = "A Nice table [" + table_name + "] is created with [" + to_string(table_columns) + "] columns.\n";
+                send(clientSocket, response.c_str(), response.length(), 0);
+            } else {
+                const char* response = "Didn't get all necessary data to create a nice table.\n";
+                send(clientSocket, response, strlen(response), 0);
             }
         }
         else if (command == "EXIT") {
