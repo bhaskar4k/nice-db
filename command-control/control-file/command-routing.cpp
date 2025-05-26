4c55096b53ca15cb093b9f3e08206a9a709f7799
bhaskar4k <bhaskarndy@gmail.com>
2025-05-26 22:10:38 +0530

    Updated

--- command-control/control-file/command-routing.cpp
+++ command-control/control-file/command-routing.cpp
@@ -32,22 +32,48 @@ void HandleCommand(SOCKET &clientSocket) {
         string command(buffer);
         command.erase(command.find_last_not_of(" \n\r\t") + 1);
 
-        if (command == "SAY_HELLO") {
-            const char* prompt = "Enter your name: ";
-            send(clientSocket, prompt, strlen(prompt), 0);
+        if (command == "CREATE") {
+            bool got_all_info_to_create_a_table = true;
+            string table_name = "";
+            int table_columns = 0;
+            
+            if ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0){
+                const char* prompt = "Table Name : ";
+                send(clientSocket, prompt, strlen(prompt), 0);
 
-            // Receive the name from the client
-            bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-            if (bytesRead > 0) {
-                buffer[bytesRead] = '\0';
-                string name(buffer);
-                name.erase(name.find_last_not_of(" \n\r\t") + 1);
+                bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+                if (bytesRead > 0) {
+                    buffer[bytesRead] = '\0';
+                    string name(buffer);
+                    name.erase(name.find_last_not_of(" \n\r\t") + 1);
+                    table_name = name;
 
-                string response = "Hello, " + name + "!\n";
-                send(clientSocket, response.c_str(), response.length(), 0);
-            } else {
-                const char* error = "No name received.\n";
-                send(clientSocket, error, strlen(error), 0);
+                    const char* prompt = "Table Columns : ";
+                    send(clientSocket, prompt, strlen(prompt), 0);
+
+                    if ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+                        buffer[bytesRead] = '\0';
+                        string column(buffer);
+                        column.erase(column.find_last_not_of(" \n\r\t") + 1);
+                        table_columns = stoi(column);
+                    } else {
+                        const char* error = "No column count received.\n";
+                        send(clientSocket, error, strlen(error), 0);
+                        got_all_info_to_create_a_table = false;
+                    } 
+                } else {
+                    const char* error = "No table name received.\n";
+                    send(clientSocket, error, strlen(error), 0);
+                    got_all_info_to_create_a_table = false;
+                }
+
+                if(got_all_info_to_create_a_table){
+                    string response = "A Nice table [" + table_name + "] created with [" + to_string(table_columns) + "] columns.\n";
+                    send(clientSocket, response.c_str(), response.length(), 0);
+                }else{
+                    string response = "Didn't get all necessery data to create a nice table.\n";
+                    send(clientSocket, response.c_str(), response.length(), 0);
+                }
             }
         }
         else if (command == "EXIT") {
@@ -56,7 +82,7 @@ void HandleCommand(SOCKET &clientSocket) {
             break;
         }
         else {
-            const char* response = "Unknown command\n";
+            const char* response = "Invalid command. Available commands: [CREATE, EXIT]\n";
             send(clientSocket, response, strlen(response), 0);
         }
     }
