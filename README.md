aab19c508473d2976aba70d3e8e1cd84fd6e6623
bhaskar4k <bhaskarndy@gmail.com>
2025-05-27 21:38:56 +0530

    Created build file

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 <p>First time in life, creating a database. :)</p>
 
 <ul>
-    <li>Build command :<code><b>$ g++ -std=c++17 main.cpp environment/env.cpp command-control/control-file/*.cpp src/db/*.cpp -Icommand-control/header-file -Ienvironment/header-file -Isrc -o main.exe -lws2_32</b></code></li>
-    <li>Run command :<code><b>$ ./main</b></code></li>
+    <li>Build & Run command (Bash) :<code><b>$ ./build.sh</b></code></li>
     <li>Open another terminal and run :<code><b>$ ncat localhost 69</b></code></li>
 </ul>
--- build.sh
+++ build.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-# Clean previous builds
-rm -rf build/
-mkdir -p build
-cd build
 
-# Configure with CMake
-cmake ..
+# Enable strict mode
+set -e
 
-# Build
-make -j$(nproc)
+# Output binary name
+OUTPUT="main.exe"
+
+# Compiler flags
+CXX_FLAGS="-std=c++17"
+
+# Include directories
+INCLUDE_DIRS="-Icommand-control/header-file -Ienvironment/header-file -Isrc"
+
+# Library flags
+LIBS="-lws2_32"
+
+# Find all .cpp files recursively
+CPP_FILES=$(find . -name "*.cpp")
+
+# Compile
+echo "Compiling..."
+g++ $CXX_FLAGS $CPP_FILES $INCLUDE_DIRS -o $OUTPUT $LIBS
+
+# Run
+echo "Running $OUTPUT"
+./$OUTPUT
